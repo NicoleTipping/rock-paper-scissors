34b5964029d092de7229c1b4eaed07433eeed667
NicoleTipping <nacayard@gmail.com>
2025-06-06 10:53:50 -0500

    add getHumanChoice function with pseudocode

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -22,3 +22,28 @@ function getComputerChoice() {
     }
 }
 console.log(getComputerChoice());
+
+// This function getHumanChoice will get the human players choice and returns one of the following string values: "rock", "paper", or "scissors"
+function getHumanChoice() {
+    let rock = "rock";
+    let paper = "paper";
+    let scissors = "scissors";
+    // SET userChoice to fill out a prompt and make a choice
+    let userChoice = prompt("Rock, Paper, or Scissors");
+    // If the userChoice equals rock
+    if(userChoice.toLowerCase() === "rock"){
+        // return rock
+        return rock;
+    }
+    // ELSE IF the userChoice equals paper
+    else if(userChoice.toLowerCase() === "paper") {
+        // return paper
+        return paper;
+    }
+    // ELSE IF the userChoice equals scissors
+    else if(userChoice.toLowerCase() === "scissors") {
+        // return scissors
+        return scissors;
+    }
+}
+console.log(getHumanChoice());
