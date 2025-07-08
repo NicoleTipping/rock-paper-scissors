1485369b1b109fbad52a5d187855e4b250354373
NicoleTipping <nacayard@gmail.com>
2025-07-08 15:16:39 -0500

    add completed buttons with event listeners for each round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,90 +1,90 @@
 // This function getComputerChoice will randomly return one of the following string values: "rock", "paper", or "scissors"
 function getComputerChoice() {
-    let rock = 'rock';
-    let paper = 'paper';
-    let scissors = 'scissors';
+    let Rock = 'Rock';
+    let Paper = 'Paper';
+    let Scissors = 'Scissors';
     // SET choice to a random whole number between 0 and 2
     let computerChoice = Math.floor(Math.random() * 3);
     // IF choice is equal to zero
     if (computerChoice == 0) {
         // return rock
-        return rock;
+        return Rock;
     }
     // ELSE IF choice is equal to one
     else if (computerChoice == 1) {
         // return paper
-        return paper;
+        return Paper;
     }
     // ELSE IF choice is equal to two
     else {
         // return scissors
-        return scissors;
+        return Scissors;
     }
 }
 // This function getHumanChoice will get the human players choice and returns one of the following string values: "rock", "paper", or "scissors"
-function getHumanChoice() {
-    let rock = 'rock';
-    let paper = 'paper';
-    let scissors = 'scissors';
-    // SET userChoice to fill out a prompt and make a choice
-    let userChoice = prompt('Rock, Paper, or Scissors');
-    // If the userChoice equals rock
-    if (userChoice.toLowerCase() === 'rock') {
-        // return rock
-        return rock;
-    }
-    // ELSE IF the userChoice equals paper
-    else if (userChoice.toLowerCase() === 'paper') {
-        // return paper
-        return paper;
-    }
-    // ELSE IF the userChoice equals scissors
-    else if (userChoice.toLowerCase() === 'scissors') {
-        // return scissors
-        return scissors;
-    }
-}
+// function getHumanChoice() {
+//     let Rock = 'Rock';
+//     let Paper = 'Paper';
+//     let Scissors = 'Scissors';
+//     // SET userChoice to fill out a prompt and make a choice
+//     let userChoice = prompt('Rock, Paper, or Scissors');
+//     // If the userChoice equals rock
+//     if (userChoice.toLowerCase() === 'Rock') {
+//         // return rock
+//         return Rock;
+//     }
+//     // ELSE IF the userChoice equals paper
+//     else if (userChoice.toLowerCase() === 'Paper') {
+//         // return paper
+//         return Paper;
+//     }
+//     // ELSE IF the userChoice equals scissors
+//     else if (userChoice.toLowerCase() === 'Scissors') {
+//         // return scissors
+//         return Scissors;
+//     }
+// }
 // This function playGame will play five rounds and keep track of the scores and declare the winner at the end
 function playGame() {
     let userScore = 0;
     let computerScore = 0;
     // This function playRound will take the human and computer choices as arguments, plays a single round, increment the round winner's score and logs a winner
     function playRound(userChoice, computerChoice) {
-        let rock = 'rock';
-        let paper = 'paper';
-        let scissors = 'scissors';
+        let Rock = 'Rock';
+        let Paper = 'Paper';
+        let Scissors = 'Scissors';
         // IF userChoice equals rock and computerChoice is equals rock
         if (userChoice === computerChoice) {
             // return "No Winner This Round"
             console.log('No Winner This Round');
         }
         // ELSE IF userChoice equals rock and computerChoice equals paper
-        else if (userChoice === rock && computerChoice === paper) {
+        else if (userChoice === Rock && computerChoice === Paper) {
             // return computerScore + 1;
             console.log('You lose! Paper beats Rock.', ++computerScore);
         }
         // ELSE IF userChoice equals rock and computerChoice equals scissors
-        else if (userChoice === rock && computerChoice === scissors) {
+        else if (userChoice === Rock && computerChoice === Scissors) {
             // return userScore + 1;
             console.log('You Win! Rock beats Scissors.', ++userScore);
         }
         // ELSE IF userChoice equals paper and computerChoice equals rock
-        else if (userChoice === paper && computerChoice === rock) {
+        else if (userChoice === Paper && computerChoice === Rock) {
             // return userScore + 1;
             console.log('You Win! Paper beats Rock.', ++userScore);
         }
         // ELSE IF userChoice equals paper and computerChoice equals scissors
-        else if (userChoice === paper && computerChoice === scissors) {
+        else if (userChoice === Paper && computerChoice === Scissors) {
             // return userScore + 1;
             console.log('You Lose! Scissors beats Paper.', ++computerScore);
         }
         // ELSE IF userChoice equals scissors and computerChoice equals rock
-        else if (userChoice === scissors && computerChoice === rock) {
+        else if (userChoice === Scissors && computerChoice === Rock) {
             // return computerScore + 1;
             console.log('You Lose! Rock beats Scissors.', ++computerScore);
         }
         // ELSE IF userChoice equals scissors and computerChoice equals paper
-        else if (userChoice === scissors && computerChoice === paper) {
+        else if (userChoice === Scissors && computerChoice === Paper) {
             // return userScore + 1;
             console.log('You Win! Scissors beats Paper.', ++userScore);
         }
@@ -103,31 +103,56 @@ function playGame() {
         };
     }
 
-    // Round 1
-    let userSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(userSelection, computerSelection);
+    const rockBtn = document.querySelector('#rock-btn');
+    
+    rockBtn.addEventListener('click', () => {
+        playRound(userChoice = "Rock", getComputerChoice());
+    });
+    
+    const paperBtn = document.querySelector('#paper-btn');
+    
+    paperBtn.addEventListener('click', () => {
+        playRound(userChoice = "Paper", getComputerChoice());
+    });
+    const scissorBtn = document.querySelector('#scissor-btn');
+    
+    scissorBtn.addEventListener('click', () => {
+        playRound(userChoice = "Scissors", getComputerChoice());
+    });
 
-    // Round 2
-    userSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(userSelection, computerSelection);
+    let body = document.querySelector('body');
 
-    // Round 3
-    userSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(userSelection, computerSelection);
+    let results = document.createElement('div');
+    body.appendChild(results);
 
-    // Round 4
-    userSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(userSelection, computerSelection);
+    results.textContent = `Your Score: ${userScore} - Computer Score: ${computerScore}`;
 
-    // Round 5
-    userSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(userSelection, computerSelection);
-    gameWinner();
+
+    // // Round 1
+    // let userSelection = getHumanChoice();
+    // let computerSelection = getComputerChoice();
+    // playRound(userSelection, computerSelection);
+
+    // // Round 2
+    // userSelection = getHumanChoice();
+    // computerSelection = getComputerChoice();
+    // playRound(userSelection, computerSelection);
+
+    // // Round 3
+    // userSelection = getHumanChoice();
+    // computerSelection = getComputerChoice();
+    // playRound(userSelection, computerSelection);
+
+    // // Round 4
+    // userSelection = getHumanChoice();
+    // computerSelection = getComputerChoice();
+    // playRound(userSelection, computerSelection);
+
+    // // Round 5
+    // userSelection = getHumanChoice();
+    // computerSelection = getComputerChoice();
+    // playRound(userSelection, computerSelection);
+    // gameWinner();
 }
 
 playGame();
